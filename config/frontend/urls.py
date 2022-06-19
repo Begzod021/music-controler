1da1757191253900680e9d7f0f6a871f794a2a3a
Begzod Rasulev <begzod_rasulev@mail.ru>
2022-06-19 17:26:21 +0500

    new

--- config/frontend/urls.py
+++ config/frontend/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import index
 urlpatterns = [
     path('', index, name="home"),
+    path('join', index),
+    path('create', index),
+    path('join/1', index)
 ]
