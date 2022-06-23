547faac3ce5da780654251ea25777bc3a9edb8ec
Begzod Rasulev <begzod_rasulev@mail.ru>
2022-06-23 17:39:14 +0500

    new

--- config/api/urls.py
+++ config/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import  path
-from .views import RoomView, CreateRoomView, GetRoom, LeaveRoom, JoinRoom, UserInRoom
+from .views import RoomView, CreateRoomView, GetRoom, LeaveRoom, JoinRoom, UserInRoom, UpdateRoom
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
@@ -7,5 +7,5 @@ urlpatterns = [
     path('leave-room',LeaveRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
     path('user-in-room', UserInRoom.as_view()),
-    
+    path('update-room', UpdateRoom.as_view()),
 ]
